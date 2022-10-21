7862f5e19634162d47e3b6acb57be3604bf04ac1
z3ro <ksw1991@gimdojin-ui-MacBookAir.local>
2022-10-21 19:00:34 +0900

    up

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,8 @@ const Home: NextPage = () => {
   const line_name:any = query.lineName
   const api_line = encodeURIComponent(line_name)
   const apikey:string|undefined = process.env.REACT_APP_APIKEY
-  const apiurl:string = 'https://subway.2ero.dev/api/subway?station=' + api_line
+  //const apiurl:string = 'https://subway.2ero.dev/api/subway?station=' + api_line
+  const apiurl:string = 'http://swopenapi.seoul.go.kr/api/subway/' + apikey + '/json/realtimePosition/0/100/' + api_line
 
   const line:any = [
     ['1호선', 1], ['2호선', 2], ['3호선', 3], ['4호선', 4], ['5호선', 5], ['6호선', 6], ['7호선', 7], ['8호선', 8], ['9호선', 9],
@@ -635,10 +636,38 @@ const Home: NextPage = () => {
     axios.get(apiurl).then((res) => {
       const data = res.data;
       if(data.status === 500) { return console.log('error!') }
-      console.log(data.realtimePositionList)
       data.realtimePositionList.map((e:any, key:number) => {
-        console.log('열차번호: ' + e['trainNo'])
-        console.log('열차종점: ' + e['statnTnm'])
+        const getStnId = e['statnId'].substr(6)
+        let stnId:string;
+        if(getStnId[0] == 0) stnId = getStnId.substr(1)
+        else stnId = getStnId
+      
+        const newDiv = document.createElement('div')
+        if(e['updnLine'] == 0) {
+          if(e['trainSttus']) {
+            document.querySelector('.stn_' + stnId)?.append('test')
+            //`[data-id=${stnId}]`
+          } else if(e['trainSttus'] === 1) {document.querySelector('.stn_' + stnId)?.append('test')
+            
+          } else {document.querySelector('.stn_' + stnId)?.append('test')
+            
+          }
+        } else {
+          if(e['trainSttus'] === 0) {
+            const newText = document.createTextNode(e['trainNo'] + '열차 ' + e['statnNm'] + ' 진입')
+            newDiv.append(newText)
+            document.querySelector('.up_approach.stnid_' + stnId)?.append(newDiv)
+          }
+          else if(e['trainSttus'] === 1) {
+            const newText = document.createTextNode(e['trainNo'] + '열차 ' + e['statnNm'] + ' 도착')
+            newDiv.append(newText)
+            document.querySelector('.up_approach.stnid_' + stnId)?.append(newDiv)
+          } else {
+            const newText = document.createTextNode(e['trainNo'] + '열차 ' + e['statnNm'] + ' 출발')
+            newDiv.append(newText)
+            document.querySelector('.up_approach.stnid_' + stnId)?.append(newDiv)
+          }
+        }
       })
     })
 
@@ -647,7 +676,10 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     if(query.lineName !== undefined && query.lineId !== undefined) {
-      const interval = setInterval(() => getData(), 30000);
+      // 최초 1회는 불러오고 이후로는 30초 단위로 갱신
+      getData()
+
+      const interval = setInterval(() => getData(), 10000);
       return() => clearInterval(interval)
     }
   })
@@ -682,15 +714,23 @@ const Home: NextPage = () => {
           eval(lineId) !== undefined && eval(lineId).map((e:any, key:number) => {
             if(e[4] !== '미개통') {
               return(
-              <div className='row' data-id={e[1]} key={key}>
-                <div className='direction_down'></div>
+              <div className='row' key={key}>
+                <div className='direction_down'>
+                  <div className='down_leave'></div>
+                  <div className='down_arrival'></div>
+                  <div className='down_approach'></div>
+                </div>
                 <div className='station_info'>
                   <div>
                     <span className={'StnId bg ' + lineId}>{e[1]}</span>
                     <span className='StnName'>{e[3]}</span>
                   </div>
                 </div>
-                <div className='direction_up'></div>
+                <div className='direction_up'>
+                  <div className={'up_leave stnid_' + e[1]}></div>
+                  <div className={'up_arrival stnid_' + e[1]}></div>
+                  <div className={'up_approach stnid_' + e[1]}></div>
+                </div>
               </div>)}
           })
         }
